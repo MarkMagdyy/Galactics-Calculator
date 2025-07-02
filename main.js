d6010b61f1c20b631c02a6ae1ad2c8cbcf54662d
Mark Magdy <120586328+MarkMagdyy@users.noreply.github.com>
2025-07-02 07:34:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function square() {
+    try {
+      const value = eval(display.value);
+      display.value = value ** 2;
+    } catch {
+      display.value = 'Error';
+    }
+  }
+
+  const display = document.getElementById("display");
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Error';
+  }
+}
